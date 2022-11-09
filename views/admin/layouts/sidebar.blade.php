9ace6717fa56b250750758102eb521b6e52ca27e
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-11-10 00:02:24 +0600

    Add files via upload

---
+++ views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,180 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url ('admin/dashboard') }}">
+                <i class="icon-grid menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        @if (Auth::guard('admin')->user()->type !== 'Vendor')
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-setting" aria-expanded="false" aria-controls="ui-setting">
+                <i class="ti-settings menu-icon"></i>
+                <span class="menu-title">Setting</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-setting">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/change-password') }}">Change Password</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/update-profile') }}">Update Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-management" aria-expanded="false" aria-controls="ui-management">
+                <i class="mdi mdi-application menu-icon"></i>
+                <span class="menu-title">Admin Management</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-management">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/admin-management/Admin') }}">Admins</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/admin-management/Subadmin') }}">Subadmins</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/admin-management/Vendor') }}">Vendors</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/admin-management/All') }}">All</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-category" aria-expanded="false" aria-controls="ui-category">
+                <i class="mdi mdi-book-multiple menu-icon"></i>
+                <span class="menu-title">Catelogue Management</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-category">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/section') }}">Sections</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/brand') }}">Brands</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/category') }}">Categories</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/product') }}">Products</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        @else
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-details" aria-expanded="false" aria-controls="ui-details">
+                <i class="mdi mdi-information-outline menu-icon"></i>
+                <span class="menu-title">Vendor Details</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-details">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/details/personal') }}">Personal Details</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/details/business') }}">Business Details</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="{{ url ('admin/details/bank') }}">Bank Details</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        @endif
+
+
+
+
+        <!-- <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+                <i class="icon-columns menu-icon"></i>
+                <span class="menu-title">Form elements</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="form-elements">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
+                <i class="icon-bar-graph menu-icon"></i>
+                <span class="menu-title">Charts</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="charts">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+                <i class="icon-grid-2 menu-icon"></i>
+                <span class="menu-title">Tables</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="tables">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+                <i class="icon-contract menu-icon"></i>
+                <span class="menu-title">Icons</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="icons">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <i class="icon-head menu-icon"></i>
+                <span class="menu-title">User Pages</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
+                <i class="icon-ban menu-icon"></i>
+                <span class="menu-title">Error pages</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="error">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pages/documentation/documentation.html">
+                <i class="icon-paper menu-icon"></i>
+                <span class="menu-title">Documentation</span>
+            </a>
+        </li> -->
+    </ul>
+</nav>
