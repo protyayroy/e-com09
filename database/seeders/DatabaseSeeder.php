930e79a69f01833ce3bc25866d10d5dba5ae5977
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-11-24 23:23:47 +0600

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+            // $this->call([AdminTableSeeder::class]);
+            // $this->call([AdminTableSeeder::class]);
+            // $this->call([VendorTableSeeder::class]);
+            // $this->call([Vendor_bank_detailTableSeeder::class]);
+            // $this->call([SectionSeeder::class]);
+            // $this->call([BrandSeeder::class]);
+            // $this->call([CategorySeeder::class]);
+
+    }
+}
